edee23cb7bee27eac97c1366f78eb80c989f9a55
Mercy Njoroge <mercykaren49@gmail.com>
2025-06-10 14:29:09 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,9 @@ load_dotenv()  # loads variables from .env
 api_key = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=api_key)
 
+if "chat_session" not in st.session_state:
+    model = genai.GenerativeModel('gemini-pro')
+    st.session_state.chat_session = model.start_chat(history=[])
 # ---- Set Page Config ----
 st.set_page_config(page_title="BitMentor", page_icon="🧠")
 
